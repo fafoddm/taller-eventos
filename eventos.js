f0230eed4659a59690059bbc0ae543af41a7a61a
fafoddm <fabriddm@gmail.com>
2023-08-13 23:49:59 -0300

    Alerta del div agregada

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let consciousDiv = document.getElementById("elDiv");
+
+     consciousDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div")
+    });
+});
